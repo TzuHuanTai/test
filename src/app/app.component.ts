a94ee2926065b3a137f30fe8ff61b54e33eda432
tairichard <Richard.tai@garmin.com>
2019-12-09 09:01:53 +0800

    chaos: test refactory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng9new';
+	title = 'ng9new';
+	resolver= [];
+
+	constructor(ComponentFactoryResolver: ComponentFactoryResolver) {
+		console.log('AppComponent');
+		let test = ComponentFactoryResolver['_factories'].forEach((element) => {
+			console.log(element)
+		});
+		this.resolver = Array.from(ComponentFactoryResolver['_factories'].values());
+	}
 }
